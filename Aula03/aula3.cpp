0a69d1fedc2c2fd1c2c8331a1b574fc52a07d350
Vitor Alves Dos Santos <161787787+VitorAlvesDevv@users.noreply.github.com>
2025-03-16 19:14:17 -0300

    Aula#03
    Exercício feito na Aula 03 do Curso de C++ do CFBCursos com professor Bruno

---
+++ Aula03/aula3.cpp
@@ -0,0 +1,16 @@
+#include <iostream> //biblioteca que usaremos
+
+using namespace std; // std= standard
+
+// main = principal
+// cout é o printf da linguagem C# 
+// endl = quebra de linha
+// \n = quebra de linha
+int main() {
+
+    cout << "Vitor Alves Desenvolvedor\nCurso de C++\n";
+
+    system("pause");
+
+    return 0; //retorno
+}
