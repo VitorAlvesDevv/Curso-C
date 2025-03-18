a868786ac22cadb3495af09acff71e11b19bbd7d
Vitor Alves Dos Santos <161787787+VitorAlvesDevv@users.noreply.github.com>
2025-03-18 19:08:55 -0300

    Aula05
    aprendemos sobre o #define

--- Aula05/aula5.cpp
+++ Aula05/aula5.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
+// #define chama sem precisar declarar no main().
+#define pi 3.1415
+#define canal cout << "Canal Vitor Alves";
 
 int main() {
 
+    int vidas=3, tiros=500, life=100;
+
+
+    cout << vidas << "\n" << tiros << "\n" << life << "\n" << pi << "\n";
+    canal 
     return 0;
 }
